8ea8ed9c7d431f6d3e01383bea9759b3d15bbd19
Lily Anderson <thatoanderson92@gmail.com>
2025-03-21 13:55:59 -0400

    add footer and favicon

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -74,21 +74,39 @@ function App() {
           <legend className="modal__legend">Select the weather type :</legend>
 
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            <input
+              id="hot"
+              name="weather"
+              type="radio"
+              className="modal__radio-input"
+            />{" "}
+            Hot
           </label>
 
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            <input
+              id="warm"
+              name="weather"
+              type="radio"
+              className="modal__radio-input"
+            />{" "}
+            Warm
           </label>
 
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            <input
+              id="cold"
+              name="weather"
+              type="radio"
+              className="modal__radio-input"
+            />{" "}
+            Cold
           </label>
         </fieldset>
       </ModalWithForm>
